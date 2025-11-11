b2f8adf304fd3148308b9914cfdba6fc790ef56c
ProfL3302 <ProfL3302@lab.aca>
2025-11-11 12:29:24 -0500

    agregue la clase login

---
+++ src/paquete/clases/Login.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete.clases;
+
+/**
+ *
+ * @author ProfL3302
+ */
+public class Login {
+    private String user;
+    private String pass;
+
+    public Login(String user, String pass) {
+        this.user = user;
+        this.pass = pass;
+    }
+    
+    
+}
