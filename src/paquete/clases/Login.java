63576b2a4076906de153c3f72174265f87560fbe
ProfL3302 <ProfL3302@lab.aca>
2025-11-11 12:33:06 -0500

    agregue los getters y setters en la clase

--- src/paquete/clases/Login.java
+++ src/paquete/clases/Login.java
@@ -16,6 +16,22 @@ public class Login {
         this.user = user;
         this.pass = pass;
     }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
     
     
 }
